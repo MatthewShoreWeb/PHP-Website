6324d5c2af59e5e9b5e28eb8161dbbc1f51f9f72
Matt Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2021-05-31 23:46:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>PHP Test Site</title>
+    <link rel='stylesheet' href='style.css'>
+    <meta charset='UTF-8'>
+</head>
+
+
+<body>
+    <header>
+
+    </header>
+    <p> Hello! </p>
+</body>
+
+</html>
