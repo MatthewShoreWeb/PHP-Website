1d05a0798887ccfdd640e85b49b9990bd3b062ee
Matt Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2021-06-01 20:59:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
+<?php 
+    $title = 'Please sign in'; 
+    include_once 'login.php';
+?>
+
 <!DOCTYPE html>
 
 <html>
 
 <head>
     <title>PHP Test Site</title>
-    <link rel='stylesheet' href='style.css'>
+    <link rel='stylesheet' type='text/css' href='style.css' />
     <meta charset='UTF-8'>
 </head>
 
 
 <body>
     <header>
-
+        <h1> <?php echo $title; ?> </h1>
     </header>
-    <p> Hello! </p>
+
+    <form id='signInForm'>
+        <div>
+            <p> Username: </p>
+            <input type='text' />
+            <p> Password: </p>
+            <input type='text' />
+            <button type='button'>Login</button> 
+        </div>
+    </form>
 </body>
 
 </html>
